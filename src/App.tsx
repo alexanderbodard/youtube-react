46b0ad5ec4f122da5885da8b9b24e27b46410d5d
Alexander Bodard <alexander.bodard@student.kuleuven.be>
2019-09-29 15:35:33 +0200

    add header navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import HeaderNav from './containers/HeaderNav/HeaderNav';
 
 const App: React.FC = () => {
   return (
-    <div>Hello World</div>
+    <HeaderNav/>
   );
 }
 
