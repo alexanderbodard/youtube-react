e10049fd45f547ec6313306a4e8aa585786b02a9
Alexander Bodard <alexander.bodard@student.kuleuven.be>
2019-09-29 19:26:18 +0200

    Add VideoPreview

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import {SideBar} from './containers/SideBar/SideBar';
 import HeaderNav from './containers/HeaderNav/HeaderNav';
+import {Home} from './containers/Home/Home';
+import './containers/Home/Home.scss';
 
 const App: React.FC = () => {
   return (
     <React.Fragment>
       <HeaderNav/>
       <SideBar/>
+      <Home/>
   </React.Fragment>
   );
 }
