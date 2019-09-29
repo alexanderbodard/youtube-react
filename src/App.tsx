65cbe196c416b79662ccbc4771ea5318086416d8
Alexander Bodard <alexander.bodard@student.kuleuven.be>
2019-09-29 16:23:37 +0200

    Add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
+import {SideBar} from './containers/SideBar/SideBar';
 import HeaderNav from './containers/HeaderNav/HeaderNav';
 
 const App: React.FC = () => {
   return (
-    <HeaderNav/>
+    <React.Fragment>
+      <HeaderNav/>
+      <SideBar/>
+  </React.Fragment>
   );
 }
 
