a77aedac373107f1ca91a27a933d11ff35febd55
Alexander Bodard <alexander.bodard@student.kuleuven.be>
2019-09-29 22:26:53 +0200

    Add video grid

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import {VideoPreview} from '../../components/VideoPreview/VideoPreview';
+import './Home.scss';
+import {VideoGrid} from '../../components/VideoGrid/VideoGrid';
+import {SideBar} from '../SideBar/SideBar';
 
 export class Home extends React.Component {
   render() {
     return (
+      <React.Fragment>
+        <SideBar/>
         <div className='home'>
-            <VideoPreview />
+          <div className="responsive-video-grid-container">
+            <VideoGrid title='Trending'/>
+            <VideoGrid title='Autos & Vehicles' hideDivider={true}/>
+          </div>
         </div>
-    )
+      </React.Fragment>
+    );
   }
 }
