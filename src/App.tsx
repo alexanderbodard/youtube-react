2e685d86780c789666400dd5a9a07cac45d1a291
Alexander Bodard <alexander.bodard@student.kuleuven.be>
2019-10-13 14:33:24 +0200

    Add watch component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import 'semantic-ui-css/semantic.min.css';
 import {AppLayout} from './components/AppLayout/AppLayout';
 import {Home} from './containers/Home/Home';
 import './containers/Home/Home.scss';
+import {Route, Switch} from 'react-router-dom';
+import { Watch } from './containers/Watch/Watch';
 
 const App: React.FC = () => {
   return (
     <AppLayout>
-      <Home/>
-  </AppLayout>
+        <Switch>
+          <Route path="/watch" component={Watch}/>
+          <Route path="/" component={Home}/>
+        </Switch>
+    </AppLayout>
   );
 }
 
